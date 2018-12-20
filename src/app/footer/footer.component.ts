1a6dd67cfc34293f70c284422f30b81f9c6b017b
Vlad Vidac <vladvidac@waterford.org>
2018-12-21 01:09:45 +0200

    add recent work

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fab} from '@fortawesome/free-brands-svg-icons';
+import {fas} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'footer',
@@ -12,6 +13,7 @@ export class FooterComponent implements OnInit {
 
   constructor() {
     library.add(fab);
+    library.add(fas);
   }
 
   ngOnInit() {
