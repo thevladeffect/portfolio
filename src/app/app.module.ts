44c07627911cb68f45912ce17b4f6b16b6c65538
Vlad Vidac <vladvidac@waterford.org>
2021-12-28 02:43:32 +0200

    Add routing; small restyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,46 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {HelloComponent} from './hello/hello.component';
 import {FooterComponent} from './footer/footer.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {HomeComponent} from './home/home.component';
+import {ArticlesComponent} from './articles/articles.component';
+import {RouterModule} from '@angular/router';
+import {MatRippleModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HelloComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
     NoopAnimationsModule,
     NgxTypedJsModule,
     FontAwesomeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'articles',
+        component: ArticlesComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        redirectTo: '/',
+        pathMatch: 'full',
+      },
+    ]),
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
